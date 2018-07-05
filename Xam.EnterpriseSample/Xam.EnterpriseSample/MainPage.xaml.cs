ab4cb0cfe3efbf16c5db859172f5d8c3db9da1e0
dbl4k <github@dbl4k.net>
2018-07-05 23:56:27 +0100

    add model and form bindings

--- Xam.EnterpriseSample/Xam.EnterpriseSample/MainPage.xaml.cs
+++ Xam.EnterpriseSample/Xam.EnterpriseSample/MainPage.xaml.cs
@@ -1,17 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Xam.EnterpriseSample.ViewModels;
 using Xamarin.Forms;
+using Newtonsoft.Json;
 
 namespace Xam.EnterpriseSample
 {
 	public partial class MainPage : ContentPage
 	{
+        private Random _random = new Random();
+        public HttpClient client = new HttpClient();
+
 		public MainPage()
 		{
 			InitializeComponent();
+
+            BindingContext = new SimpleViewModel();
 		}
+
+        public SimpleViewModel Model
+        {
+            get
+            {
+                return (SimpleViewModel)BindingContext;
+            }
+            
+        }
+
+        public async void Button_OnClick(object sender, EventArgs args)
+        {
+            var id = _random.Next(1, 10);
+            var response = await client.GetAsync("http://jsonplaceholder.typicode.com/posts/" + id.ToString());
+            var body = await response.Content.ReadAsStringAsync();
+            var post = JsonConvert.DeserializeObject<DataContracts.TypicodePost>(body);
+            
+            Model.Id = post.Id;
+            Model.Title = post.Title;
+            Model.Description = post.Body;
+        }
 	}
 }
---
+++ Xam.EnterpriseSample/Xam.EnterpriseSample/ViewModels/SimpleViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Xam.EnterpriseSample.ViewModels
+{
+    public class SimpleViewModel : INotifyPropertyChanged
+    {
+        private int _id;
+
+        public int Id 
+        { 
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string _title;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string _description;
+
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                _description = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        #region "INotifyPropertyChanged"
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion 
+    }
+}
