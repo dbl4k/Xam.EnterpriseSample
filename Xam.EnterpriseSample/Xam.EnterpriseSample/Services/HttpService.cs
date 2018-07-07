cdc19422944f753b498973f369d956bf4eddc487
dbl4k <github@dbl4k.net>
2018-07-07 10:15:15 +0100

    move httpservice into singleton

---
+++ Xam.EnterpriseSample/Xam.EnterpriseSample/Services/HttpService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+namespace Xam.EnterpriseSample.Services
+{
+    // singleton http instance wrapper
+    class HttpService
+    {
+        private static readonly HttpClient _client = new HttpClient();
+
+        public static HttpClient Client
+        {
+            get
+            {
+                return _client;
+            }
+        }
+
+        public HttpService()
+        {
+
+        }
+    }
+}
